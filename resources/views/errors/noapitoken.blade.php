062c1c599c9f06c194ad19ca27e23642741451e8
ViolanteCodes <maria@buttercms.com>
2022-04-15 11:16:05 -0400

    update API token instructions

--- resources/views/errors/noapitoken.blade.php
+++ resources/views/errors/noapitoken.blade.php
@@ -6,7 +6,7 @@
             <div class="col-xl-6 col-lg-6 col-md-10">
                 <div class="hero-content">
                     <h1>Configure your ButterCMS API Token</h1>
-                    <p>Please add your API token to your <i>.env</i> file.</p>
+                    <p>Please add your ButterCMS API token to your <i>.env</i> file as <i>LARAVEL_APP_BUTTER_CMS_API_KEY</i>.</p>
                     <a target="_blank" rel="noreferrer" href="https://buttercms.com/join/" class="main-btn btn-hover">
                         Get your free API token
                     </a>
